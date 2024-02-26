d2a468318a3d825c10b78c51a9ec66ea72b6ea53
techArthouse <art.aguilar003@gmail.com>
2024-02-26 13:56:45 -0800

    Initial Commit

---
+++ UIDemo/UIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIDemoApp.swift
+//  UIDemo
+//
+//  Created by Arturo Aguilar on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct UIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
