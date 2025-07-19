c8ed84a3da1813c3108439dba91c374d797bb08b
techArthouse <art.aguilar003@gmail.com>
2025-07-18 21:36:33 -0500

    created custom logger to replace debug print statements and not have to remove them anytime we go to prod

---
+++ UIDemo/Logger.swift
@@ -0,0 +1,39 @@
+//
+//  Logger.swift
+//  UIDemo
+//
+//  Created by Arturo Aguilar on 7/18/25.
+//
+
+
+import Foundation
+
+enum LogLevel: String {
+    case debug = "🐛"
+    case info = "ℹ️"
+    case warning = "⚠️"
+    case error = "🚨"
+}
+
+struct Logger {
+    static var isEnabled: Bool = {
+#if DEBUG
+        return true
+#else
+        return false
+#endif
+    }()
+    
+    static func log(_ level: LogLevel = .debug,
+                    tag: String? = nil,
+                    _ message: @autoclosure () -> String,
+                    file: String = #file,
+                    function: String = #function,
+                    line: Int = #line) {
+        guard isEnabled else { return }
+        
+        let fileName = (file as NSString).lastPathComponent
+        let tagString = tag.map { "[\($0)]" } ?? ""
+        print("\(level.rawValue) \(tagString) [\(fileName):\(line)] \(function) — \(message())")
+    }
+}
